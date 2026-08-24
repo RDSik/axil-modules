544b40556ca6271663f738ce658f29c0ad7908c9
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-08-24 14:30:31 +0300

    Update axil_top.sv

--- modules/top/rtl/axil_top.sv
+++ modules/top/rtl/axil_top.sv
@@ -134,7 +134,6 @@ module axil_top #(
     );
 
     axil_rgmii #(
-        .CLK_FREQ       (CLK_FREQ),
         .AXIL_ADDR_WIDTH(AXIL_ADDR_WIDTH),
         .AXIL_DATA_WIDTH(AXIL_DATA_WIDTH),
         .RGMII_WIDTH    (RGMII_WIDTH),
